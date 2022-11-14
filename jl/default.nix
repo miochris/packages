5cc91159249c570c9eb3e9d0fecb37a4709a29f5
qiang <qiang.liu@smarkets.com>
2022-11-14 22:06:19 +0000

    added go package jl

---
+++ jl/default.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixpkgs> {} }: {
+  jl = pkgs.buildGoModule rec {
+    pname = "jl";
+    version = "1.4.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "koenbollen";
+      repo = "jl";
+      rev = "v${version}";
+      sha256 = "sha256-cEFbCvOkYNbYxQC6q4Jrl1jR48gXskBts9OTUEgnXoY=";
+    };
+    vendorSha256 = "sha256-mRyMZq+MhPLplrZCR/AzWb48VhVEYBKftwlDZFIU7ug=";
+  };
+}
